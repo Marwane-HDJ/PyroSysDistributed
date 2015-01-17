63fb0e31d53882626f74a7361bb290ba6b97d47d
MonssefLeGris <mfaghloumi@gmail;com>
2015-01-17 17:32:32 +0100

    testPush

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 # saved as client.py
 import Pyro4
 
-#demonstration of use: client
+#demonstration of  use: client
 filesContainer=Pyro4.Proxy("PYRONAME:example.filesContainer")    # use name server object lookup uri shortcut
 print filesContainer.take("make1.txt")
