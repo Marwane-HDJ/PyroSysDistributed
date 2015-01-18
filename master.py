18ac27f91cdde8742cf7db6bcfd62f9fe24761df
Marouane <Marouane>
2015-01-18 14:17:34 +0100

    renaming

--- master.py
+++ master.py
@@ -8,7 +8,6 @@ import threading
 class Master(object):
     def __init__(self):
         self.workers = []
-        self.workerName = ""
         # queue of tasks to be done, the master should put the tasks here as they are available to run
         self.toBeDone = Queue.Queue()
         for i in range(100):
@@ -17,21 +16,21 @@ class Master(object):
     def list_contents(self):
         return self.workers
 
-    def register(self, workerName):  # register the worker and send work to it
-        self.workers.append(workerName)
-        print("worker " + workerName + " registred.")
-        self.sendWork(workerName)
+    def register(self, worker_name):  # register the worker and send work to it
+        self.workers.append(worker_name)
+        print("worker " + worker_name + " registred.")
+        self.send_work(worker_name)
 
     def echo(self, message):
         print message
         return "Hi slave"
 
-    def sendWork(self, workerName):
-        worker = Pyro4.Proxy("PYRONAME:" + workerName)
+    def send_work(self, worker_name):
+        worker = Pyro4.Proxy("PYRONAME:" + worker_name)
         while True:  # send work forever
-            #print (" sending work")
-            result = (worker.doWork(self.toBeDone.get(block=True)))  #block until there is some work to do
-            #Do something with the result
+            # print (" sending work")
+            result = (worker.doWork(self.toBeDone.get(block=True)))  # block until there is some work to do
+            # Do something with the result
 
 
 def main():
--- worker.py
+++ worker.py
@@ -8,19 +8,18 @@ class Worker(object):
     def __init__(self, name):
         self.name = name
 
-
     def register(self, master):
         print("Worker {0} want to register.".format(self.name))
         master.register(self.name)
         print("Worker {0} registred".format(self.name))
 
-    def doWork(self, commandAndFiles):
-        print (commandAndFiles)
+    def do_work(self, command_files):
+        print (command_files)
         time.sleep(1)
         # return the resulting files
         return "I'm ready to work more, my master"
 
-    def registerAtNameServer(self):
+    def register_at_nameserver(self):
         print ("registering worker at NameServer")
         Pyro4.Daemon.serveSimple(
             {
@@ -35,11 +34,11 @@ def main():
 
 
     # Use another thread to register itself
-    t1 = threading.Thread(target=worker.registerAtNameServer)
+    t1 = threading.Thread(target=worker.register_at_nameserver)
     t1.start()
     print("Passed here ")
 
-    #register itself at the master and it will send work automatically
+    # register itself at the master and it will send work automatically
     master = Pyro4.Proxy("PYRONAME:master")
     master.register(worker.name)
     t1.join()
